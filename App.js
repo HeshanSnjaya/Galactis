12c4baa081dc9856fb2b87240ce94beb67601688
Navindu <dahanayakenw.20@uom.lk>
2023-08-20 18:21:45 +0530

    landing page and login

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from "react";
+import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
+import { EvaIconsPack } from "@ui-kitten/eva-icons";
+import * as eva from "@eva-design/eva";
+
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import LandingScreen from "./app/screens/LandingScreen";
+import LoginScreen from "./app/screens/LoginScreen";
+import HomeScreen from "./app/screens/HomeScreen";
+
+import * as Font from "expo-font";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   const [fontsLoaded, setFontsLoaded] = useState(false);
+
+   const loadFonts = async () => {
+      await Font.loadAsync({
+         Goldman: require('./assets/fonts/Goldman-Regular.ttf'),
+         GoldmanBold: require("./assets/fonts/Goldman-Bold.ttf"),
+      });
+   };
+
+   useEffect(() => {
+      loadFonts().then(() => {
+         setFontsLoaded(true);
+      });
+   }, []);
+
+   if (!fontsLoaded) {
+      // Return a loading screen or null until fonts are loaded
+      return null;
+   }
+
+   return (
+      <React.Fragment>
+         <IconRegistry icons={EvaIconsPack} />
+         <ApplicationProvider {...eva} theme={{ ...eva.light }}>
+            <NavigationContainer>
+               <Stack.Navigator>
+                  <Stack.Screen
+                     options={{ headerShown: false }}
+                     name="Landing"
+                     component={LandingScreen}
+                  />
+                  <Stack.Screen
+                     options={{ headerShown: false }}
+                     name="Login"
+                     component={LoginScreen}
+                  />
+                  <Stack.Screen name="Home" component={HomeScreen} />
+               </Stack.Navigator>
+            </NavigationContainer>
+         </ApplicationProvider>
+      </React.Fragment>
+   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+   container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+   },
 });
